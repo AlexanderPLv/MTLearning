63946e54c7957c2937b9da08b222685d482fd0a7
Alexander Pelevinov <43724686+AlexanderPLv@users.noreply.github.com>
2023-04-09 14:32:07 +0300

    Initial commit

---
+++ MTLearning/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MTLearning
+//
+//  Created by Alexander Pelevinov on 09.04.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
