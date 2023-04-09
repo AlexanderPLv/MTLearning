16e1605a469304f4842dc495552d1912b7080b6a
Alexander Pelevinov <43724686+AlexanderPLv@users.noreply.github.com>
2023-04-09 19:59:50 +0300

    setup+clearColor

--- MTLearning/ViewController.swift
+++ MTLearning/ViewController.swift
@@ -5,22 +5,47 @@
 //  Created by Alexander Pelevinov on 09.04.2023.
 //
 
-import Cocoa
+import Metal
+import MetalKit
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var metalView: MTKView!
+    var device: MTLDevice!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        device = MTLCreateSystemDefaultDevice()
+        metalView.device = device
+        metalView.delegate = self
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
 
+extension ViewController: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
+    
+    func draw(in view: MTKView) {
+        metalView.clearColor = MTLClearColor(
+            red: 0.0, green: 0.5, blue: 1.0, alpha: 1.0
+        )
+        
+        let commandQueue = device.makeCommandQueue()!
+        let commandBuffer = commandQueue.makeCommandBuffer()!
+        
+        guard let renderPassDescriptor = view.currentRenderPassDescriptor else {
+            print("Didn't get a render pass descriptor from MTKView; dropping frame...")
+            return
+        }
+        let renderPassEncoder = commandBuffer.makeRenderCommandEncoder(
+            descriptor: renderPassDescriptor
+        )!
+        renderPassEncoder.endEncoding()
+        
+        
+        commandBuffer.present(view.currentDrawable!)
+        commandBuffer.commit()
+    }
+    
+}
