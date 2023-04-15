6086742cf4fbfa849cef9eac79ea70186cfa0d75
Alexander Pelevinov <43724686+AlexanderPLv@users.noreply.github.com>
2023-04-15 12:16:21 +0300

    Simple Mesh

--- MTLearning/Metal/Shaders.metal
+++ MTLearning/Metal/Shaders.metal
@@ -20,7 +20,7 @@ struct VertexOut {
 
 vertex VertexOut vertex_main(
     VertexIn in [[stage_in]],
-    constant float4x4 &transform [[buffer(1)]]
+    constant float4x4 &transform [[buffer(2)]]
     ) {
     VertexOut out;
         out.position = transform * float4(in.position, 0.0, 1.0);
--- MTLearning/Renderer/Renderer.swift
+++ MTLearning/Renderer/Renderer.swift
@@ -23,8 +23,9 @@ class Renderer: NSObject {
     private var renderingConstants: RenderingConstants
     private var constantsBuffer: MTLBuffer!
     
-    private var time: TimeInterval = 0.0
+    private let mesh: SimpleMesh
     
+    private var time: TimeInterval = 0.0
     private var frameSemaphore = DispatchSemaphore(value:
         MaxOutstandingFrameCount)
  
@@ -33,6 +34,10 @@ class Renderer: NSObject {
         self.device = device
         self.commandQueue = device.makeCommandQueue()!
         self.renderingConstants = RenderingConstants()
+        self.mesh = SimpleMesh(indexedPlanarPolygonSideCount: 8,
+                               radius: 250,
+                               color: SIMD4<Float>(0.0, 0.5, 0.8, 1.0),
+                               device: device)
         super.init()
         view.device = device
         view.delegate = self
@@ -53,7 +58,7 @@ private extension Renderer {
             fatalError("Unable to create default Metal library")
         }
         let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
-        renderPipelineDescriptor.vertexDescriptor = createVertexDescriptor()
+        renderPipelineDescriptor.vertexDescriptor = mesh.vertexDescriptor
         
         renderPipelineDescriptor.vertexFunction = library.makeFunction(
             name: "vertex_main"
@@ -72,19 +77,11 @@ private extension Renderer {
     }
     
     func makeResources() {
-        var vertexData: [Float] = [
-        //    x     y       r    g    b    a
-            -100,  -20,    1.0, 0.0, 1.0, 1.0,
-             100,  -60,    0.0, 1.0, 1.0, 1.0,
-              30,  100,    1.0, 1.0, 0.0, 1.0,
-        ]
-        vertexBuffer = device.makeBuffer(
-            bytes: &vertexData,
-            length: MemoryLayout<Float>.stride * vertexData.count,
-            options: .storageModeShared)
         constantsBuffer = device.makeBuffer(
             length: renderingConstants.constantsStride * MaxOutstandingFrameCount,
-            options: .storageModeShared)
+            options: .storageModeShared
+        )
+        constantsBuffer.label = "Dynamic Constant Buffer"
     }
     
 }
@@ -98,7 +95,7 @@ extension Renderer: MTKViewDelegate{
     func draw(in view: MTKView) {
         frameSemaphore.wait()
         updateConstants()
-        drawTriangle()
+        drawSimpleMesh()
     }
     
 }
@@ -106,54 +103,36 @@ extension Renderer: MTKViewDelegate{
 //MARK: - Graphics Pipeline
 
 private extension Renderer {
- 
-    func drawTriangle() {
+    
+    func drawSimpleMesh() {
         guard let renderPassDescriptor = view.currentRenderPassDescriptor else { return }
+        
         guard let commandBuffer = commandQueue.makeCommandBuffer() else { return }
         
-        let renderCommandEncoder = commandBuffer.makeRenderCommandEncoder(
-            descriptor: renderPassDescriptor
-        )!
+        let renderCommandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
         renderCommandEncoder.setRenderPipelineState(renderPipelineState)
-        renderCommandEncoder.setVertexBuffer(
-            vertexBuffer,
-            offset: 0,
-            index: 0
-        )
-        renderCommandEncoder.setVertexBuffer(
-            constantsBuffer,
-            offset: renderingConstants.currentConstantBufferOffset,
-            index: 1
-        )
-        renderCommandEncoder.drawPrimitives(
-            type: .triangle,
-            vertexStart: 0,
-            vertexCount: 3
-        )
+        
+        
+        
+        for (i, vertexBuffer) in mesh.vertexBuffers.enumerated() {
+            renderCommandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: i)
+        }
+        renderCommandEncoder.setVertexBuffer(constantsBuffer, offset: renderingConstants.currentConstantBufferOffset, index: 2)
+        
+        renderCommandEncoder.drawIndexedPrimitives(type: mesh.primitiveType,
+                                                   indexCount: mesh.indexCount,
+                                                   indexType: mesh.indexType,
+                                                   indexBuffer: mesh.indexBuffer,
+                                                   indexBufferOffset: 0)
         renderCommandEncoder.endEncoding()
         
         commandBuffer.present(view.currentDrawable!)
         commandBuffer.addCompletedHandler { [weak self] _ in
-                self?.frameSemaphore.signal()
-            }
+            self?.frameSemaphore.signal()
+        }
         commandBuffer.commit()
+        
         renderingConstants.frameIndex += 1
-    }
-    
-    func createVertexDescriptor() -> MTLVertexDescriptor {
-        let vertexDescriptor = MTLVertexDescriptor()
-        
-        vertexDescriptor.attributes[0].format = .float2
-        vertexDescriptor.attributes[0].offset = 0
-        vertexDescriptor.attributes[0].bufferIndex = 0
-        
-        vertexDescriptor.attributes[1].format = .float4
-        vertexDescriptor.attributes[1].offset = MemoryLayout<Float>.stride * 2
-        vertexDescriptor.attributes[1].bufferIndex = 0
-        
-        vertexDescriptor.layouts[0].stride = MemoryLayout<Float>.stride * 6
-        
-        return vertexDescriptor
     }
     
     func updateConstants() {
@@ -188,86 +167,14 @@ private extension Renderer {
     }
     
     func makeModelMatrix() -> float4x4 {
-        time += 1.0 / Double(view.preferredFramesPerSecond)
         let floatTime = Float(time)
-        
-        let pulseRate: Float = 1.5
-        let scaleFactor = 1.0 + 0.5 * cos(pulseRate * floatTime)
-        let scale = SIMD2<Float>(scaleFactor, scaleFactor)
-        let scaleMatrix = simd_float4x4(scale2D: scale)
         
         let rotationRate: Float = 2.5
         let rotationAngle = rotationRate * floatTime
         let rotationMatrix = simd_float4x4(rotateZ: rotationAngle)
         
-        let orbitalRadius: Float = 200
-        let translation = orbitalRadius * SIMD2<Float>(cos(floatTime), sin(floatTime))
-        let translationMatrix = simd_float4x4(translate2D: translation)
-        
-        let modelMatrix = translationMatrix * rotationMatrix * scaleMatrix
+        let modelMatrix = rotationMatrix
         return modelMatrix
-    }
-    
-}
-
-//MARK: - ComputePipeline
-
-private extension Renderer {
-    
-    func addArrays() {
-        guard let library = device.makeDefaultLibrary() else {
-            fatalError("Unable to create default shader library")
-        }
-        
-        for name in library.functionNames {
-            let function = library.makeFunction(name: name)!
-            print("\(function)")
-        }
-        
-        let kernelFunction = library.makeFunction(name: "add_two_values")!
-        let computePipeline = try! device.makeComputePipelineState(function: kernelFunction)
-        
-        let threadsPerThreadgroup = MTLSize(width: 32, height: 1, depth: 1)
-        let threadgroupCount = MTLSize(width: 8, height: 1, depth: 1)
-        let elementCount = 256
-        let inputBufferA = device.makeBuffer(
-            length: MemoryLayout<Float>.stride * elementCount, options: .storageModeShared
-        )!
-        let inputBufferB = device.makeBuffer(
-            length: MemoryLayout<Float>.stride * elementCount, options: .storageModeShared
-        )!
-        let outputBuffer = device.makeBuffer(
-            length: MemoryLayout<Float>.stride * elementCount, options: .storageModeShared
-        )!
-        
-        let inputsA = inputBufferA.contents().assumingMemoryBound(to: Float.self)
-        let inputsB = inputBufferB.contents().assumingMemoryBound(to: Float.self)
-        for i in 0..<elementCount {
-            inputsA[i] = Float(i)
-            inputsB[i] = Float(elementCount - i)
-        }
-        let commandBuffer = commandQueue.makeCommandBuffer()!
-        let commandEncoder = commandBuffer.makeComputeCommandEncoder()!
-        
-        commandEncoder.setComputePipelineState(computePipeline)
-        
-        commandEncoder.setBuffer(inputBufferA, offset: 0, index: 0)
-        commandEncoder.setBuffer(inputBufferB, offset: 0, index: 1)
-        commandEncoder.setBuffer(outputBuffer, offset: 0, index: 2)
-        
-        commandEncoder.dispatchThreadgroups(
-            threadgroupCount,
-            threadsPerThreadgroup: threadsPerThreadgroup
-        )
-        commandEncoder.endEncoding()
-        
-        commandBuffer.addCompletedHandler { _ in
-            let outputs = outputBuffer.contents().assumingMemoryBound(to: Float.self)
-            for i in 0..<elementCount {
-                print("Output element \(i) is \(outputs[i])")
-            }
-        }
-        commandBuffer.commit()
     }
     
 }
